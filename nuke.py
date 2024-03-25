5a356b6a1cfe0a9b345f0827a287e634b3f14d83
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-03-25 17:54:40 +0530

    added 2 bit nuking

--- nuke.py
+++ nuke.py
@@ -6,14 +6,20 @@ class NukeFiles:
     def __init__(self, days: int = 30, path: str = "logs/"):
         self.__days = days
         self.__path = path
-        self.__time_now = datetime.datetime.now()
+        self.__time_now = datetime.datetime.today()
 
     def nuke_files(self):
         for subdirs, dirs, files in os.walk(self.__path):
             for file in files:
                 file_path = os.path.join(subdirs, file)
                 if os.path.isfile(file_path):
+                    file_day = int(file.split("-")[0])
+                    today = str(self.__time_now.day)
+                    day_dif = abs(int(today) - file_day)
                     file_origin_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
                     if self.__time_now - file_origin_time > datetime.timedelta(days=self.__days):
+                        print(file_path)
+                        os.remove(file_path)
+                    elif day_dif >= self.__days:
                         print(file_path)
                         os.remove(file_path)
